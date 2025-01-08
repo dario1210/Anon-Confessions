974fc9bb1e8dda2a4ef6afc7944c16b3c9ddde5a
user3306 <dariopipa@rovigosolutions.co.uk>
2025-01-08 03:14:53 +0100

    Add ON DELETE CASCADE to foreign key constraints in likes and comments tables

--- cmd/internal/db/migrations_files/000003_create_ulikes_table.up.sql
+++ cmd/internal/db/migrations_files/000003_create_ulikes_table.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE posts_likes (
     post_id INTEGER NOT NULL,              
     user_id INTEGER NOT NULL,               
     PRIMARY KEY (post_id, user_id),
-    FOREIGN KEY (post_id) REFERENCES posts(id) 
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
--- cmd/internal/db/migrations_files/000004_create_comments_table.up.sql
+++ cmd/internal/db/migrations_files/000004_create_comments_table.up.sql
@@ -4,5 +4,6 @@ CREATE TABLE comments (
     post_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (post_id) REFERENCES posts(id)
+    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
